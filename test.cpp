7b51aee54911f13a09ac39725796e15043e6f0a4
1404Samyak <mahapatra.samyak@gmail.com>
2025-08-03 18:15:11 +0530

    commit

--- test.cpp
+++ test.cpp
@@ -2,34 +2,30 @@
 using namespace std;
 typedef long long ll;
 int main() {
-    ll n;
+    int n;
     cin>>n;
-    vector<ll>a(n);
-    for(ll i=0;i<n;i++)cin>>a[i];
-    set<int>s;
-    vector<vector<ll>>temp(n);
-    vector<vector<ll>>ans;
-    ll ind=-1;
-    for(ll i=0;i<n;i++){
-        if(s.find(a[i])!=s.end())continue;
-        else ind++;
-        ll last=1e8;
-        for(ll j=i;j<n;j++){
-            if(a[j]<last && s.find(a[j])==s.end()){
-                temp[ind].push_back(a[j]);
-                last=a[j];
-                s.insert(a[j]);
-            }
+    vector<int>nums(n);
+    for(int i=0;i<n;i++)cin>>nums[i];
+    int cnt=1,prev=nums[0];
+    for(int i=1;i<n;i++){
+        if(nums[i]==prev){
+            cnt++;
+            prev=nums[i];
+            if(cnt>2)nums[i]=-1;
+        }
+        else{
+            cnt=1;
+            prev=nums[i];
         }
     }
+    int j=0;
     for(int i=0;i<n;i++){
-        if(temp[i].size()>0)ans.push_back(temp[i]);
-    }
-    for(auto vec:ans){
-        for(ll num:vec){
-            cout<<num<<" ";
+        if(nums[i]>=0){
+            swap(nums[i],nums[j]);
+            j++;
         }
-        cout<<endl;
     }
+    for(int i:nums)cout<<i<<" ";
+    cout<<endl;
     return 0;
 }
