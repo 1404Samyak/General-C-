2c7b640f1a20bcb73c58b85fa97b5e340f930383
1404Samyak <mahapatra.samyak@gmail.com>
2025-10-06 01:43:37 +0530

    commit

--- test.cpp
+++ test.cpp
@@ -2,30 +2,28 @@
 using namespace std;
 typedef long long ll;
 int main() {
-    int n;
-    cin>>n;
-    vector<int>nums(n);
-    for(int i=0;i<n;i++)cin>>nums[i];
-    int cnt=1,prev=nums[0];
-    for(int i=1;i<n;i++){
-        if(nums[i]==prev){
-            cnt++;
-            prev=nums[i];
-            if(cnt>2)nums[i]=-1;
+    ll n,k;
+    cin>>n>>k;
+    vector<ll>a(n);
+    for(ll i=0;i<n;i++)cin>>a[i];
+    map<ll,ll>mp;
+    ll ans=0;
+    //basically i want distinct pairs (i,j) such that (a[i]+a[j])%k==0
+    //for ex in 1 2 2 3 2 4 10(k=2) the number of pairs are index wise are (1,2),(0,3),(4,5) only so 3 pairs
+    for(ll i=0;i<n;i++){
+        ll rem=a[i]%k;
+        mp[rem]++;
+    }
+    //so rem can be 0 1 2 ... k-1
+    for(ll r=0;r<k;r++){
+        ll complement=(k-r)%k; //to handle case when r=0
+        if(r==complement){ //means r=0 or r=k/2 when k is even
+            ans+=mp[r]/2; //we can make pairs within this group only
         }
-        else{
-            cnt=1;
-            prev=nums[i];
+        else if(r<complement){ //to avoid double counting
+            ans+=min(mp[r],mp[complement]); //we can make pairs between these two groups only
         }
     }
-    int j=0;
-    for(int i=0;i<n;i++){
-        if(nums[i]>=0){
-            swap(nums[i],nums[j]);
-            j++;
-        }
-    }
-    for(int i:nums)cout<<i<<" ";
-    cout<<endl;
+    cout<<2*ans<<"\n";
     return 0;
 }
