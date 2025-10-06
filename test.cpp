69c6bc08073fcb21dd3b4bcbf9005eed8c62af3d
1404Samyak <mahapatra.samyak@gmail.com>
2025-10-06 15:50:59 +0530

    commit

--- test.cpp
+++ test.cpp
@@ -4,26 +4,21 @@ typedef long long ll;
 int main() {
     ll n,k;
     cin>>n>>k;
-    vector<ll>a(n);
-    for(ll i=0;i<n;i++)cin>>a[i];
-    map<ll,ll>mp;
-    ll ans=0;
-    //basically i want distinct pairs (i,j) such that (a[i]+a[j])%k==0
-    //for ex in 1 2 2 3 2 4 10(k=2) the number of pairs are index wise are (1,2),(0,3),(4,5) only so 3 pairs
-    for(ll i=0;i<n;i++){
-        ll rem=a[i]%k;
-        mp[rem]++;
-    }
-    //so rem can be 0 1 2 ... k-1
-    for(ll r=0;r<k;r++){
-        ll complement=(k-r)%k; //to handle case when r=0
-        if(r==complement){ //means r=0 or r=k/2 when k is even
-            ans+=mp[r]/2; //we can make pairs within this group only
+    vector<ll> a(n);
+    for(ll i=0;i<n;i++) cin>>a[i];
+    ll sum=0,total=0;
+    ll l=0,r=0;
+    while(r<n){
+        sum=sum+a[r];
+        if(r-l+1==k){
+            total=total+sum;
+            sum=sum-a[l];
+            l++;
         }
-        else if(r<complement){ //to avoid double counting
-            ans+=min(mp[r],mp[complement]); //we can make pairs between these two groups only
-        }
+        r++;
     }
-    cout<<2*ans<<"\n";
+    double ans=0.0;
+    ans=(double)total/(n-k+1);
+    cout<<fixed<<setprecision(9)<<ans<<endl;
     return 0;
 }
