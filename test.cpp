cf22d69e3b6546fc77e0d5134129184e11cc5afb
1404Samyak <mahapatra.samyak@gmail.com>
2025-10-10 21:12:00 +0530

    Commit

--- test.cpp
+++ test.cpp
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int main() {
-    ll n,k;
-    cin>>n>>k;
-    vector<ll> a(n);
-    for(ll i=0;i<n;i++) cin>>a[i];
-    ll sum=0,total=0;
-    ll l=0,r=0;
-    while(r<n){
-        sum=sum+a[r];
-        if(r-l+1==k){
-            total=total+sum;
-            sum=sum-a[l];
-            l++;
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &i : a) cin >> i;
+    long long sum = 0, need = 0;
+    for (int i = 0; i < n; ++i) {
+        need += i;
+        //because our best possible case is 0,1,2,3,4,5...i.e each position has at least i elements to its left so that it can be swapped to leftmost position
+        //such that entire seq is strictly increasing last element can be as big as possible so at any moment if sum<need we can say no
+        //as we can never make it strictly increasing
+        sum += a[i];
+        if (sum < need) {
+            cout << "NO\n";
+            return;
         }
-        r++;
     }
-    double ans=0.0;
-    ans=(double)total/(n-k+1);
-    cout<<fixed<<setprecision(9)<<ans<<endl;
+    cout << "YES\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
     return 0;
 }
